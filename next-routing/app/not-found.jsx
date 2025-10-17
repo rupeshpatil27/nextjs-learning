72121e93c2ba7132e4fd5a280b87e4eb63b69e60
rupeshpatil <rupeshpatil88927@gmail.com>
2025-10-17 23:58:42 +0530

    implemented custom 404 page to handle invalid routes

---
+++ next-routing/app/not-found.jsx
@@ -0,0 +1,11 @@
+export default async function NotFound() {
+  
+
+  return (
+    <div className="main-container space-y-2">
+      <div className="bg-[#fcfbfb] flex justify-start gap-5 flex-col p-10 rounded-md">
+        <span className="text-2xl">Not Found!</span>
+      </div>
+    </div>
+  );
+}
