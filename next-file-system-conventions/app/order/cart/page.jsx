6f1914b94f66e2327773a94e9a5419be83876de3
rupeshpatil <rupeshpatil88927@gmail.com>
2025-10-26 23:27:48 +0530

    Implement error handling and recovery in Next.js

--- next-file-system-conventions/app/order/cart/page.jsx
+++ next-file-system-conventions/app/order/cart/page.jsx
@@ -1,10 +1,19 @@
-
 export const metadata = {
   title: "cart Page",
   description: "cart details",
 };
 
+function getRandomInt(num) {
+  return Math.floor(Math.random() * num);
+}
+
 export default function Cart() {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error !.. - in Cart Page");
+  }
+
   return (
     <div className="main-container">
       <h1 className="text-4xl">Cart Page</h1>
